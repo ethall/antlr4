b98b64bb77c4a36d0945eb22c0e6476ba55f5a58
Evan Hall <evan.tom.hall@gmail.com>
2019-10-19 14:47:22 -0400

    Added another environment file

---
+++ env2.ps1
@@ -0,0 +1,4 @@
+$env:CLASSPATH=".;C:\Source\antlr4\tool\target\antlr4-4.7.3-SNAPSHOT-complete.jar"
+$env:JAVA_HOME="C:\jdk-13.0.1\"
+$env:PATH="C:\maven\v3.6.2\bin;$env:PATH"
+$env:MAVEN_OPTS="-Xmx1G"
